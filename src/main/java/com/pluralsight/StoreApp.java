592aeef6834e47514ef98924745ae54c07841580
Reggie <reglitster00@gmail.com>
2025-10-08 10:49:10 -0400

    Commit 2

--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,13 +8,47 @@ public class StoreApp {
     public static void main(String[] args) {
 
         ArrayList<Product> inventory = getInventory();
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("We carry the following inventory: ");
         for (Product p : inventory) {
             System.out.printf("id: %d %s - Price: $%.2f\n",
                     p.getId(), p.getName(), p.getPrice());
         }
+        try{
+            FileReader fileReader = new FileReader("src/main/resources/inventory.csv");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+
+            int count = 0;
+            String input;
+            while ((input = bufReader.readLine()) != null) {
+                String[] parts = input.split("\\|");
+
+                int id = Integer.parseInt(parts[0]);
+                String productName = parts[1];
+                float price = Float.parseFloat(parts[2]);
+
+                Product product = new Product(id, productName, price);
+                //String formattedProduct = String.format("ID: %d | Product: %s | Price: %.2f", id, productName, price);
+                System.out.printf("id: %d %s - Price: $%.2f\n", id, productName, price);
+
+                count++;
+
+
+            }
+
+
+
+
+
+
+        } catch (IOException e){
+            System.out.println("An Unexpected Error Has Occurred.");
+            e.printStackTrace();
+            }
+
+
+
     }
 
     public static ArrayList<Product> getInventory() {
@@ -26,6 +61,7 @@ public class StoreApp {
         inventory.add(new Product(12, "Cheese", 12.00f));
         inventory.add(new Product(53, "BBQ Sauce", 3.00f));
         inventory.add(new Product(41, "Milk", 15.00f));
+
 
         return inventory;
     }
