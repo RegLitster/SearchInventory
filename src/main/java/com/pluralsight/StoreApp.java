1abe791c71a1f272a2998228d498cca8b4521681
Reggie <reglitster00@gmail.com>
2025-10-08 11:59:57 -0400

    Lunch Commit

--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -8,17 +8,48 @@ import java.util.Scanner;
 
 public class StoreApp {
     public static void main(String[] args) {
-
-       ArrayList<Product> inventory = getInventory();
         Scanner scanner = new Scanner(System.in);
 
-        inventory.sort(Comparator.comparingInt(Product::getId));
+    while (true) {
+        System.out.println("""
+                Welcome to the Store App Please Make a Following Selection\s
+                1- List all products
+                2- Lookup a product by its id
+                3- Find all products within a price range
+                4- Add a new product
+                5- Quit the application""");
+        int input = scanner.nextInt();
+        scanner.nextLine();
 
-        for (Product p : inventory) {
-            System.out.printf("id: %d %s - Price: $%.2f\n",
-                    p.getId(), p.getName(), p.getPrice());
+        ArrayList<Product> inventory = getInventory();
+
+        switch (input) {
+            case 1:
+                inventory.sort(Comparator.comparingInt(Product::getId));
+                for (Product p : inventory) {
+                    System.out.printf("id: %d %s - Price: $%.2f\n",
+                            p.getId(), p.getName(), p.getPrice());
+                }
+                System.out.println("Press ENTER to continue...");
+                scanner.nextLine();
+                break;
+            case 2:
+                System.out.println("Enter the product id:");
+                int id = scanner.nextInt();
+                scanner.nextLine();
+                for (Product p : inventory) {
+                    if (p.getId() == id) {
+                        System.out.printf("id: %d %s - Price: $%.2f\n",p.getId(), p.getName(), p.getPrice());
+                    }else {
+                        System.out.println("Invalid product id!");
+                    }
+                }
+                System.out.println("Press ENTER to continue...");
+                scanner.nextLine();
+                break;
+
         }
-
+    }
     }
 
     public static ArrayList<Product> getInventory() {
@@ -26,7 +57,6 @@ public class StoreApp {
         ArrayList<Product> inventory = new ArrayList<Product>();
 // this method loads product objects into inventory
 // and its details are not shown
-
         try{
             FileReader fileReader = new FileReader("src/main/resources/inventory.csv");
             BufferedReader bufReader = new BufferedReader(fileReader);
@@ -45,13 +75,10 @@ public class StoreApp {
               inventory.add(product);
             }
 
-
         } catch (IOException e){
             System.out.println("An Unexpected Error Has Occurred.");
             e.printStackTrace();
         }
-
-
         return inventory;
     }
 }
